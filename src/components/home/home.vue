8247148ea43a9ec586aea14e838ed5faf7823ac8
yinhang <demohang@163.com>
2017-10-27 11:01:44 +0800

    添加文件

---
+++ src/components/home/home.vue
@@ -0,0 +1,267 @@
+<template>
+  <div class="home">
+    <section class="section_introduce">
+      <div class="introduce_title">We Are 伐木累</div>
+      <div class="introduce_explain">一群逗比青年的聚集地</div>
+      <p class="text">long long ago,有着一群活泼可爱的青年，他们洋溢着青春的笑容。</p>
+      <p class="text">but..</p>
+      <p class="text">他们走上了一条与众不同的道路，变得屌丝了起来</p>
+      <p class="text">————程序员的道路————</p>
+    </section>
+    <section class="section_message" id="section_message">
+      <div class="message_title">We Are...</div>
+      <div class="message_portrait_ulBox">
+        <ul class="message_portrait_list">
+          <li>
+            <div class="message_portrait"></div>
+            <div class="message_introduction">
+              <p>小普</p>
+              <p>前端中级工程师（一个屌丝）</p>
+              <p>对vue,node有着深入的研究</p>
+            </div>
+          </li>
+          <li>
+            <div class="message_portrait"></div>
+            <div>aaaa</div>
+          </li>
+        </ul>
+      </div>
+    </section>
+    <section class="section_time">
+      <div class="time_title">我们的相识</div>
+      <svg width="100%" height="100%" viewBox="0 30 400 300" class="letters letters--effect-3">
+					<!--J-->
+					<g class="letter letter--1">
+						<g class="letter__part">
+							<path class="letter__layer" d="M20.4,52.1c17.9,0.2,46.3,1.2,63.3-4.4C73,72.5,68.1,114.4,66.3,141.3c-0.2,3-0.6,6.3-2.7,8.4c-3,3-8,2.2-11.9,0.7c-5.4-2.2-10.3-5.7-13.4-10.6c-3.1-4.9-4.2-11.2-2.1-16.7c1.7-4.5,5.4-8.1,9.6-10.5s8.9-3.8,13.6-5.1c16-4.4,32.2-7.9,48.6-10.4" />
+							<path class="letter__layer" d="M20.4,52.1c17.9,0.2,46.3,1.2,63.3-4.4C73,72.5,68.1,114.4,66.3,141.3c-0.2,3-0.6,6.3-2.7,8.4c-3,3-8,2.2-11.9,0.7c-5.4-2.2-10.3-5.7-13.4-10.6c-3.1-4.9-4.2-11.2-2.1-16.7c1.7-4.5,5.4-8.1,9.6-10.5s8.9-3.8,13.6-5.1c16-4.4,32.2-7.9,48.6-10.4" />
+							<path class="letter__layer" d="M20.4,52.1c17.9,0.2,46.3,1.2,63.3-4.4C73,72.5,68.1,114.4,66.3,141.3c-0.2,3-0.6,6.3-2.7,8.4c-3,3-8,2.2-11.9,0.7c-5.4-2.2-10.3-5.7-13.4-10.6c-3.1-4.9-4.2-11.2-2.1-16.7c1.7-4.5,5.4-8.1,9.6-10.5s8.9-3.8,13.6-5.1c16-4.4,32.2-7.9,48.6-10.4" />
+						</g>
+					</g>
+					<!--u-->
+					<g class="letter letter--2">
+						<g class="letter__part">
+							<path class="letter__layer" d="M93.2,83.7c8.8-6.2,17.5-12.4,26.3-18.6c-0.2,11.9-0.1,23.7,0.1,35.6c0.1,2.5,0.7,5.8,3.2,6.2c1,0.2,2.1-0.2,3-0.6c14.6-6.7,22.9-22.4,27.6-37.8c-0.6,7.1-1.2,14.2-0.1,21.2c1.1,7,4.1,13.9,9.5,18.5" />
+							<path class="letter__layer" d="M93.2,83.7c8.8-6.2,17.5-12.4,26.3-18.6c-0.2,11.9-0.1,23.7,0.1,35.6c0.1,2.5,0.7,5.8,3.2,6.2c1,0.2,2.1-0.2,3-0.6c14.6-6.7,22.9-22.4,27.6-37.8c-0.6,7.1-1.2,14.2-0.1,21.2c1.1,7,4.1,13.9,9.5,18.5" />
+							<path class="letter__layer" d="M93.2,83.7c8.8-6.2,17.5-12.4,26.3-18.6c-0.2,11.9-0.1,23.7,0.1,35.6c0.1,2.5,0.7,5.8,3.2,6.2c1,0.2,2.1-0.2,3-0.6c14.6-6.7,22.9-22.4,27.6-37.8c-0.6,7.1-1.2,14.2-0.1,21.2c1.1,7,4.1,13.9,9.5,18.5" />
+						</g>
+					</g>
+					<!--s-->
+					<g class="letter letter--3">
+						<g class="letter__part">
+							<path class="letter__layer" d="M207.4,86.7c0.6-3.1,1.3-6.5-0.3-9.4c-2.9-5.4-11-5.7-16.4-5.6c-3.9,0.1-7.9,0.8-11.4,2.5c-3.5,1.8-6.5,4.8-7.8,8.6c-0.8,2.6-0.7,5.8,1.4,7.5c0.8,0.6,1.7,1,2.7,1.3c4.7,1.4,9.6,0.5,14.5,0.1s10.1-0.2,14,2.8c4.9,3.8,5.8,11.8,2,16.6c-2.9,3.6-7.6,5.1-12.1,5.5c-5.5,0.5-11-0.5-16-2.8c-2.6-1.2-5.3-3-5.9-5.9" />
+							<path class="letter__layer" d="M207.4,86.7c0.6-3.1,1.3-6.5-0.3-9.4c-2.9-5.4-11-5.7-16.4-5.6c-3.9,0.1-7.9,0.8-11.4,2.5c-3.5,1.8-6.5,4.8-7.8,8.6c-0.8,2.6-0.7,5.8,1.4,7.5c0.8,0.6,1.7,1,2.7,1.3c4.7,1.4,9.6,0.5,14.5,0.1s10.1-0.2,14,2.8c4.9,3.8,5.8,11.8,2,16.6c-2.9,3.6-7.6,5.1-12.1,5.5c-5.5,0.5-11-0.5-16-2.8c-2.6-1.2-5.3-3-5.9-5.9" />
+							<path class="letter__layer" d="M207.4,86.7c0.6-3.1,1.3-6.5-0.3-9.4c-2.9-5.4-11-5.7-16.4-5.6c-3.9,0.1-7.9,0.8-11.4,2.5c-3.5,1.8-6.5,4.8-7.8,8.6c-0.8,2.6-0.7,5.8,1.4,7.5c0.8,0.6,1.7,1,2.7,1.3c4.7,1.4,9.6,0.5,14.5,0.1s10.1-0.2,14,2.8c4.9,3.8,5.8,11.8,2,16.6c-2.9,3.6-7.6,5.1-12.1,5.5c-5.5,0.5-11-0.5-16-2.8c-2.6-1.2-5.3-3-5.9-5.9" />
+						</g>
+					</g>
+					<!--t-->
+					<g class="letter letter--4">
+						<g class="letter__part">
+							<path class="letter__layer" d="M281.1,127.4c-7.2,5.6-13,10.5-22.7,11.1c-8.5,0.5-17.2-2.7-23.6-8.2c-6.5-5.6-8.2-15.4-8-23.5c0.5-17.8,2.9-35.5,7.2-52.8" />
+							<path class="letter__layer" d="M281.1,127.4c-7.2,5.6-13,10.5-22.7,11.1c-8.5,0.5-17.2-2.7-23.6-8.2c-6.5-5.6-8.2-15.4-8-23.5c0.5-17.8,2.9-35.5,7.2-52.8" />
+							<path class="letter__layer" d="M281.1,127.4c-7.2,5.6-13,10.5-22.7,11.1c-8.5,0.5-17.2-2.7-23.6-8.2c-6.5-5.6-8.2-15.4-8-23.5c0.5-17.8,2.9-35.5,7.2-52.8" />
+						</g>
+						<g class="letter__part">
+							<path class="letter__layer" d="M217.1,90.5c14.6,0.2,29.3,0,43.9-0.7" />
+							<path class="letter__layer" d="M217.1,90.5c14.6,0.2,29.3,0,43.9-0.7" />
+							<path class="letter__layer" d="M217.1,90.5c14.6,0.2,29.3,0,43.9-0.7" />
+						</g>
+					</g>
+					<!--S-->
+					<g class="letter letter--5">
+						<g class="letter__part">
+							<path class="letter__layer" d="M127.8,141.5c-1.3-0.2-4.3-0.6-7.2,1c-5.2,2.8-7.4,10.1-5.2,14.2c3,5.8,14.2,5.3,21.2,2.1c1.6-0.7,8.2-3.8,10.3-10.6c2.7-8.9-4.7-19-12-23.9c-6.4-4.3-14.6-6-22-3.8c-7.7,2.3-13.9,8.4-17.9,15.4c-8.1,14.2-10,30.6-3.9,45.9c2.9,7.4,8.3,13.8,12.7,20.4c4.3,6.5,10.5,15.3,12.1,23.3c5.9,29.5-32.8,30.9-50.8,21.7c-7.8-4-13-9.6-16.1-15.9c-7.5-15.4,2.2-37.9,17.6-43.2c6-2.1,11.7-1.2,15.6-0.2" />
+							<path class="letter__layer" d="M127.8,141.5c-1.3-0.2-4.3-0.6-7.2,1c-5.2,2.8-7.4,10.1-5.2,14.2c3,5.8,14.2,5.3,21.2,2.1c1.6-0.7,8.2-3.8,10.3-10.6c2.7-8.9-4.7-19-12-23.9c-6.4-4.3-14.6-6-22-3.8c-7.7,2.3-13.9,8.4-17.9,15.4c-8.1,14.2-10,30.6-3.9,45.9c2.9,7.4,8.3,13.8,12.7,20.4c4.3,6.5,10.5,15.3,12.1,23.3c5.9,29.5-32.8,30.9-50.8,21.7c-7.8-4-13-9.6-16.1-15.9c-7.5-15.4,2.2-37.9,17.6-43.2c6-2.1,11.7-1.2,15.6-0.2" />
+							<path class="letter__layer" d="M127.8,141.5c-1.3-0.2-4.3-0.6-7.2,1c-5.2,2.8-7.4,10.1-5.2,14.2c3,5.8,14.2,5.3,21.2,2.1c1.6-0.7,8.2-3.8,10.3-10.6c2.7-8.9-4.7-19-12-23.9c-6.4-4.3-14.6-6-22-3.8c-7.7,2.3-13.9,8.4-17.9,15.4c-8.1,14.2-10,30.6-3.9,45.9c2.9,7.4,8.3,13.8,12.7,20.4c4.3,6.5,10.5,15.3,12.1,23.3c5.9,29.5-32.8,30.9-50.8,21.7c-7.8-4-13-9.6-16.1-15.9c-7.5-15.4,2.2-37.9,17.6-43.2c6-2.1,11.7-1.2,15.6-0.2" />
+						</g>
+					</g>
+					<!--m-->
+					<g class="letter letter--6">
+						<g class="letter__part">
+							<path class="letter__layer" d="M205.3,237.9c1.8-17.6,3.6-35.4,0.6-52.8c-0.3-1.5-0.8-3.2-2.3-3.5c-0.7-0.1-1.4,0.2-2.1,0.5c-9.3,4.1-17.8,10.2-24.8,17.6" />
+							<path class="letter__layer" d="M205.3,237.9c1.8-17.6,3.6-35.4,0.6-52.8c-0.3-1.5-0.8-3.2-2.3-3.5c-0.7-0.1-1.4,0.2-2.1,0.5c-9.3,4.1-17.8,10.2-24.8,17.6" />
+							<path class="letter__layer" d="M205.3,237.9c1.8-17.6,3.6-35.4,0.6-52.8c-0.3-1.5-0.8-3.2-2.3-3.5c-0.7-0.1-1.4,0.2-2.1,0.5c-9.3,4.1-17.8,10.2-24.8,17.6" />
+						</g>
+						<g class="letter__part">
+							<path class="letter__layer" d="M174.8,237.3c1.8-17.6,3.6-35.4,0.6-52.8c-0.3-1.5-0.8-3.2-2.3-3.5c-0.7-0.1-1.4,0.2-2.1,0.5c-9.3,4.1-17.8,10.2-24.8,17.6" />
+							<path class="letter__layer" d="M174.8,237.3c1.8-17.6,3.6-35.4,0.6-52.8c-0.3-1.5-0.8-3.2-2.3-3.5c-0.7-0.1-1.4,0.2-2.1,0.5c-9.3,4.1-17.8,10.2-24.8,17.6" />
+							<path class="letter__layer" d="M174.8,237.3c1.8-17.6,3.6-35.4,0.6-52.8c-0.3-1.5-0.8-3.2-2.3-3.5c-0.7-0.1-1.4,0.2-2.1,0.5c-9.3,4.1-17.8,10.2-24.8,17.6" />
+						</g>
+						<g class="letter__part">
+							<path class="letter__layer" d="M116.4,198c7-7.4,15.5-13.4,24.8-17.6c0.7-0.3,1.4-0.6,2.1-0.5c1.5,0.3,2,2,2.3,3.5c3,17.4,1.2,35.2-0.6,52.8" />
+							<path class="letter__layer" d="M116.4,198c7-7.4,15.5-13.4,24.8-17.6c0.7-0.3,1.4-0.6,2.1-0.5c1.5,0.3,2,2,2.3,3.5c3,17.4,1.2,35.2-0.6,52.8" />
+							<path class="letter__layer" d="M116.4,198c7-7.4,15.5-13.4,24.8-17.6c0.7-0.3,1.4-0.6,2.1-0.5c1.5,0.3,2,2,2.3,3.5c3,17.4,1.2,35.2-0.6,52.8" />
+						</g>
+					</g>
+					<!--o-->
+					<g class="letter letter--7">
+						<g class="letter__part">
+							<path class="letter__layer" d="M224.7,214.5c-1.6,25.1,46.6,25.5,35.2-13.9C251.5,171.6,226.4,188.7,224.7,214.5z" />
+							<path class="letter__layer" d="M224.7,214.5c-1.6,25.1,46.6,25.5,35.2-13.9C251.5,171.6,226.4,188.7,224.7,214.5z" />
+							<path class="letter__layer" d="M224.7,214.5c-1.6,25.1,46.6,25.5,35.2-13.9C251.5,171.6,226.4,188.7,224.7,214.5z" />
+						</g>
+					</g>
+					<!--k-->
+					<g class="letter letter--8">
+						<g class="letter__part">
+							<path class="letter__layer" d="M266.8,188.2c0.7,1.4,7.1,13.8,21.2,16.1c11.3,1.9,19.6-4.2,22.2-5.8c5.9-3.6,12.9-12.1,12.7-19.4c-0.4-11.6-15.7-7.5-22.2-4.8c-10.3,4.3-17.8,11.6-18,25.8c-0.2,13.2,24.7,43.2,49.3,24.5" />
+							<path class="letter__layer" d="M266.8,188.2c0.7,1.4,7.1,13.8,21.2,16.1c11.3,1.9,19.6-4.2,22.2-5.8c5.9-3.6,12.9-12.1,12.7-19.4c-0.4-11.6-15.7-7.5-22.2-4.8c-10.3,4.3-17.8,11.6-18,25.8c-0.2,13.2,24.7,43.2,49.3,24.5" />
+							<path class="letter__layer" d="M266.8,188.2c0.7,1.4,7.1,13.8,21.2,16.1c11.3,1.9,19.6-4.2,22.2-5.8c5.9-3.6,12.9-12.1,12.7-19.4c-0.4-11.6-15.7-7.5-22.2-4.8c-10.3,4.3-17.8,11.6-18,25.8c-0.2,13.2,24.7,43.2,49.3,24.5" />
+						</g>
+						<g class="letter__part">
+							<path class="letter__layer" d="M175.8,138.2c11.1,17.1,28.4,27.9,48.6,30.7c16.8,2.3,34-0.4,50.1-5.3c9.7-3,19.4-6.6,27.7-12.4c10.4-7.3,10.4-7.3,20-18.3c10.1-11.6,6.8-25.6-8.3-24.2c-8.4,0.8-16.4,8.7-20.2,16.4c-4,8.1-4.4,17.7-5,26.5c-0.7,9.5-2.6,18.7-4,28.1c-2.6,18.2-4.7,36.5-6.4,54.8" />
+							<path class="letter__layer" d="M175.8,138.2c11.1,17.1,28.4,27.9,48.6,30.7c16.8,2.3,34-0.4,50.1-5.3c9.7-3,19.4-6.6,27.7-12.4c10.4-7.3,10.4-7.3,20-18.3c10.1-11.6,6.8-25.6-8.3-24.2c-8.4,0.8-16.4,8.7-20.2,16.4c-4,8.1-4.4,17.7-5,26.5c-0.7,9.5-2.6,18.7-4,28.1c-2.6,18.2-4.7,36.5-6.4,54.8" />
+							<path class="letter__layer" d="M175.8,138.2c11.1,17.1,28.4,27.9,48.6,30.7c16.8,2.3,34-0.4,50.1-5.3c9.7-3,19.4-6.6,27.7-12.4c10.4-7.3,10.4-7.3,20-18.3c10.1-11.6,6.8-25.6-8.3-24.2c-8.4,0.8-16.4,8.7-20.2,16.4c-4,8.1-4.4,17.7-5,26.5c-0.7,9.5-2.6,18.7-4,28.1c-2.6,18.2-4.7,36.5-6.4,54.8" />
+						</g>
+					</g>
+					<!--e-->
+					<g class="letter letter--9">
+						<g class="letter__part">
+							<path class="letter__layer" d="M335.6,204.7c51.4,1.2,40.3-43.1,10.9-22.9c-28,19.3,0.9,66.8,33.1,38.7" />
+							<path class="letter__layer" d="M335.6,204.7c51.4,1.2,40.3-43.1,10.9-22.9c-28,19.3,0.9,66.8,33.1,38.7" />
+							<path class="letter__layer" d="M335.6,204.7c51.4,1.2,40.3-43.1,10.9-22.9c-28,19.3,0.9,66.8,33.1,38.7" />
+						</g>
+					</g>
+				</svg>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    methods: {
+      handleScroll () {
+        // console.log(document.getElementById('section_message').offsetTop - window.scrollY)
+      }
+    },
+    created: function () {
+      window.addEventListener('scroll', this.handleScroll)
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style rel="stylesheet/scss" lang="scss" scoped>
+  section {
+      position: relative;
+      margin: 0;
+      opacity: 1;
+      overflow: hidden;
+      color: #fff;
+      height: 100vh;
+      width: 100vw;
+      -webkit-transition: opacity 2s ease;
+      -moz-transition: opacity 2s ease;
+      -ms-transition: opacity 2s ease;
+      -o-transition: opacity 2s ease;
+      transition: opacity 2s ease;
+      user-select: none;
+  }
+  .section_introduce {
+      background: url(http://v5.res.dandyweng.com/images/backgrounds/intro.jpg);
+      background-size: cover;
+      background-attachment: fixed;
+      background-position: bottom center;
+      background-repeat: no-repeat;
+      text-align: center;
+      .introduce_title {
+          text-shadow: 0 2px 5px rgba(0,0,0,0.3);
+          letter-spacing: 1px;
+          font-size: 36px;
+          margin-top: 150px;
+      }
+      .introduce_explain {
+          margin-top: 3px;
+          margin-bottom: 25px;
+      }
+      .text{
+          line-height: 30px;
+          font-size: 16px;
+      }
+  }
+  .section_message {
+      background: url(http://v5.res.dandyweng.com/images/backgrounds/homeschool.jpg);
+      background-size: cover;
+      background-attachment: fixed;
+      background-position: bottom center;
+      background-repeat: no-repeat;
+      text-align: center;
+      .message_title {
+          text-shadow: 0 2px 5px rgba(0,0,0,0.3);
+          letter-spacing: 1px;
+          font-size: 36px;
+          margin-top: 70px;
+      }
+      .message_portrait_ulBox {
+          width: 100%;
+          margin-top: 100PX;
+          text-align: center;
+      }
+      .message_portrait_list {
+          display: inline-block;
+          li {
+              float: left;
+              margin: 0 50px;
+              text-align: center;
+              .message_portrait {
+                  width: 120px;
+                  height: 120px;
+                  border-radius: 100%;
+                  box-shadow: 0 0 0 2px rgba(255,255,255,0.5), 0px 2px 18px 3px rgba(0,0,0,0.2);
+                  background-color: rgba(255,255,255,0.3);
+                  background-image: url('../../assets/headPortrait/xiaopu.jpeg');
+                  background-size: cover;
+                  background-position: -10px -35px;;
+              }
+              .message_introduction {
+                  width: 120px;
+                  margin-top: 20px;
+                  p {
+                      float: left;
+                      width: 40px;
+                      writing-mode:tb-rl;
+                      line-height: 40px;
+                      font-size: 14px;
+                  }
+              }
+          }
+      }
+  }
+  .section_time {
+    background: url('../../assets/backgrounds/timeBg.jpg');
+    background-size: cover;
+    // background-attachment: fixed;
+    background-position: bottom center;
+    background-repeat: no-repeat;
+    text-align: center;
+    .time_title {
+      text-shadow: 0 2px 5px rgba(0,0,0,0.3);
+      letter-spacing: 1px;
+      font-size: 36px;
+      margin-top: 120px;
+    }
+    .letters--effect-3 .letter__layer:first-child {
+      -webkit-transform: translate3d(-2px, -2px, 0);
+      transform: translate3d(-2px, -2px, 0);
+      stroke: #dddde6;
+      stroke-width: 10px;
+    }
+
+    .letters--effect-3 .letter__layer:nth-child(2) {
+      stroke: #eca80e;
+      stroke-width: 6px;
+    }
+
+    .letters--effect-3 .letter__layer:nth-child(3) {
+      stroke: #607d8b;
+      stroke-width: 8px;
+    }
+    .letter__layer {
+      fill: none;
+      stroke-miterlimit: 3;
+      stroke-linecap: butt;
+      stroke-linejoin: bevel;
+    }
+  }
+</style>
